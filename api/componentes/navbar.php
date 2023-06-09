b75872e17eb3bbb318f6c21f9d55b8b92e5625e1
RafaRamos31 <rafaelramos19972@gmail.com>
2023-06-09 11:31:45 -0600

    initial commit

---
+++ api/componentes/navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/api">
+          <img src="recursos/logo.png" width="90" height="30" class="d-inline-block align-top" alt="">
+          Nutrición | Copán
+        </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/api/noticias.php">Noticias</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/api/contacto.php">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/api/publicar.php">Recursos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sitios de Interes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"> 
+          <button type="button" class="btn btn-warning">
+            <i class="bi bi-door-open-fill"></i>
+            Gestión
+          </button>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
